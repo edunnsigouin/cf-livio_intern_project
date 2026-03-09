4729940dda0f471f4b05f5532beb049b0c87a090
Etienne Dunn-Sigouin <Etienne.Dunn-Sigouin@uib.no>
2026-03-09 14:09:47 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "livio_intern_project",
+    version = "0.0.1",
+    author = "Etienne Dunn-Sigouin",
+    packages=['livio_intern_project'],
+    install_requires=["numpy","xarray","scipy"],
+)
+
